5622147c16dc42fed96bc614298c57a5704f63a2
farhanganteng@gmail.com <farhanganteng@gmail.com>
2025-09-22 12:33:06 +0700

    oke

--- server.js
+++ server.js
@@ -1,27 +1,57 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3300;
 
 app.use(express.json());
 
 let movies = [
-  { id: 1, film_id:'86e544fd-79de-4e04-be62-5be67d8dd92e', user: 'Cimeng', rating: 5, comment:'Film terbaik sepanjang masa'},
-  { id: 2, film_id:'2baf70d1-42bb-4437-b551-e5fed5a87abe', user: 'Pohan', rating: 5, comment:'Film terbaik sepanjang masa'},
-  { id: 3, film_id:'12cfb892-aac0-4c5b-94af-521852e46d6a', user: 'Dipa', rating: 5, comment:'Film terbaik sepanjang masa'},
-  { id: 4, film_id:'58611129-2dbc-4a81-a72f-77ddfc1b1b49', user: 'Anggi',rating: 5, comment:'Film terbaik sepanjang masa'}
+  {
+    id: 1,
+    film_id: "86e544fd-79de-4e04-be62-5be67d8dd92e",
+    user: "Cimeng",
+    rating: 5,
+    comment: "Film terbaik sepanjang masa",
+  },
+  {
+    id: 2,
+    film_id: "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+    user: "Pohan",
+    rating: 5,
+    comment: "Film terbaik sepanjang masa",
+  },
+  {
+    id: 3,
+    film_id: "12cfb892-aac0-4c5b-94af-521852e46d6a",
+    user: "Dipa",
+    rating: 5,
+    comment: "Film terbaik sepanjang masa",
+  },
+  {
+    id: 4,
+    film_id: "58611129-2dbc-4a81-a72f-77ddfc1b1b49",
+    user: "Anggi",
+    rating: 5,
+    comment: "Film terbaik sepanjang masa",
+  },
 ];
 
 //endpoint
-app.get('/', (req, res) => {
-    res.send('Movie API is running');
-})
+app.get("/", (req, res) => {
+  res.send("Movie API is running");
+});
 
 //GET status
-app.get('/status', (req, res) => {
-    res.json(status)
-})
+app.get("/status", (req, res) => {
+  res.json(movies);
+});
 
+//GET reviews
+app.get("/reviews", (req, res) => {
+  res.json(reviews);
+});
 
+//GET reviews id
+app;
 
 //start the server
 app.listen(PORT, () => {
